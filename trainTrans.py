fd1910fa555c0ddcff330ad03c73173ded01463e
Mahdi <mahdi.alehdaghi@gmail.com>
2022-01-01 00:43:51 -0500

    Adds Cross modal matching module

--- trainTrans.py
+++ trainTrans.py
@@ -231,7 +231,7 @@ if len(args.resume) > 0:
         start_epoch = checkpoint['epoch']
         net.load_state_dict(checkpoint['net'])
         print('==> loaded checkpoint {} (epoch {} Rank-1: {:.2%} mAP: {:.2%})'
-              .format(args.resume, checkpoint['epoch'],checkpoint['cmc'], checkpoint['mAP']))
+              .format(args.resume, checkpoint['epoch'], checkpoint['cmc'].item(), checkpoint['mAP'].item()))
     else:
         print('==> no checkpoint found at {}'.format(args.resume))
 
